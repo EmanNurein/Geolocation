5b7881372e11967095dbac9b802213edec44fb07
EmanNurein <eamw96:gmail.com>
2022-03-11 16:28:24 +0200

    First Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function changeMode(){
+    var mybody= document.body;
+    mybody.classList.toggle('mydark')
+}
+function loadCoupon(){
+    document.getElementById('coupon').style.visibility ='visible';
+
+}
+
+function closeCoupon(){
+    document.getElementById('coupon').style.visibility ='hidden';
+}
