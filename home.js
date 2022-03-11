36dd7c8dd7acd5248fc7ad5d34658ba5c5745893
EmanNurein <eamw96:gmail.com>
2022-03-11 16:31:57 +0200

    file modified

--- home.js
+++
@@ -1,9 +0,0 @@
-
-function loadCoupon(){
-    document.getElementById('coupon').style.visibility ='visible';
-
-}
-
-function closeCoupon(){
-    document.getElementById('coupon').style.visibility ='hidden';
-}
